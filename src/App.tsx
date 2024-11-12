c92ac36fa82b6079b3369b84e5074ab7e194373f
Sorbon <mark.jonsan@bk.ru>
2024-11-12 16:25:32 +0500

    Grid System added for responsive layout

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+/**
+ //* @format
+ */
+
+function App() {
+  return (
+    <>
+
+<main className="container m-auto template gap-y-3 gap-x-12">
+<header className="bg-green-400 header">Header</header>
+
+<aside className="bg-green-400 aside">
+  Aside
+  <nav>
+   Nav
+  </nav>
+</aside>
+
+<section className=" bg-orange-300 content">Content</section>
+
+<footer className="bg-green-400 footer">
+  Footer
+</footer>
+
+</main>
+    </>
+  );
+}
+
+export default App;
